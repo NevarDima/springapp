01c963487cae75db6b7266b84ead28bbd157ac4c
Dima <nevar.dima@gmail.com>
2019-08-05 22:02:53 +0300

    add springapp

---
+++ src/main/java/by/nevar/springapp/TestSpring.java
@@ -0,0 +1,13 @@
+package by.nevar.springapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean1 = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean1.getName());
+        context.close();
+    }
+}
